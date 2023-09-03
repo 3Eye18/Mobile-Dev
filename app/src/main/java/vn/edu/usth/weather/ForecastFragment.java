83899b402e808d08b21de37b61f2327462e7f4d6
3Eye18 <ngvu182002@gmail.com>
2023-09-03 21:22:20 +0700

    Labwork 4: Changed TextView and added ImageView (with the sunny.png) to the ForecastFragment

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,12 +60,12 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
         // Set the background color of the view
-        view.setBackgroundColor(Color.parseColor("#FF0000"));
+        view.setBackgroundColor(Color.parseColor("#a0eef2"));
         return view;
     }
+
 }
