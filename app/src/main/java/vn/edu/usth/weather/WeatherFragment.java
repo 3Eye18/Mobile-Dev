7d3a2d187a0ca5fb25e2cb3747a17bc958ccf3c9
3Eye18 <ngvu182002@gmail.com>
2023-09-12 00:22:23 +0700

    Labwork 6: Added WeatherFragment right above ForecastFragment

--- app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -8,18 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-public class ForecastFragment extends Fragment {
+public class WeatherFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        View view = inflater.inflate(R.layout.fragment_weather, container, false);
         // Set the background color of the view
-        view.setBackgroundColor(Color.parseColor("#8DDFCB"));
+        view.setBackgroundColor(Color.parseColor("#ECEE81"));
         return view;
     }
-
 }
